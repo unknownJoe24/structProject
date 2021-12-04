d1903d79a17169547bd191167ce9d384f60ee3f9
unknownJoe24 <56737384+unknownJoe24@users.noreply.github.com>
2021-12-04 18:10:35 -0500

    Started work

---
+++ Fibonacci.cpp
@@ -0,0 +1,11 @@
+//program written in C++ that returns the n-th Fibonacci term
+
+#include <iostream>
+
+
+
+int main()
+{
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
---
+++ Fibonacci.py
@@ -0,0 +1,7 @@
+#Program written in Python that returns the n-th Fibonacci term
+
+nterm = int(input("Enter Fibonacci term: "))
+
+n0, n1 = 0, 1
+term = 0
+
