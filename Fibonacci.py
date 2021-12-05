8a91cda7805937251f66bebde391fc1128cd635a
unknownJoe24 <56737384+unknownJoe24@users.noreply.github.com>
2021-12-04 21:41:31 -0500

    finished python version

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,7 +1,19 @@
-#Program written in Python that returns the n-th Fibonacci term
+#Program written in Python that returns Fibonacci sequence up to the n-th term
 
 nterm = int(input("Enter Fibonacci term: "))
 
 n0, n1 = 0, 1
 term = 0
 
+if nterm <= 0:
+    print("Pleas enter a positive integer")
+elif nterm == 1:
+    print("Fibonacci term at ", nterm, ":")
+    print(n0)
+else: #while loop to print out the sequence until the n-th term is reached
+    while term < nterm:
+        print(n0)
+        nth = n0 + n1
+        n0 = n1
+        n1 = nth
+        term += 1
